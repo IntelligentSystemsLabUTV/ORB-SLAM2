7e61a74b429b2d202b5bd6954e9becacef829d59
Roberto Masocco <robmasocco@gmail.com>
2023-07-02 15:26:09 +0200

    feat: config, launch files

---
+++ src/orbslam2_driver/launch/orb2_driver_zedm_stereo.launch.py
@@ -0,0 +1,52 @@
+"""
+ORB-SLAM2 Driver app launch file.
+
+Roberto Masocco <robmasocco@gmail.com>
+Intelligent Systems Lab <isl.torvergata@gmail.com>
+
+July 2, 2023
+"""
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Build config file path
+    config = os.path.join(
+        get_package_share_directory('orbslam2_driver'),
+        'config',
+        'orb2_driver_zedm_stereo_vga.yaml')
+
+    # Declare launch arguments
+    ns = LaunchConfiguration('namespace')
+    cf = LaunchConfiguration('cf')
+    ns_launch_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='orbslam2')
+    cf_launch_arg = DeclareLaunchArgument(
+        'cf',
+        default_value=config)
+    ld.add_action(ns_launch_arg)
+    ld.add_action(cf_launch_arg)
+
+    # Create node launch description
+    node = Node(
+        package='orbslam2_driver',
+        executable='orbslam2_driver_app',
+        namespace=ns,
+        emulate_tty=True,
+        output='both',
+        log_cmd=True,
+        parameters=[cf])
+
+    ld.add_action(node)
+
+    return ld
